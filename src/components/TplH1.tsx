807e903c9c8d8465f9e548835a45eae0291c6470
mako0523 <kurimako.0523@gmail.com>
2024-05-02 21:42:20 +0900

    fix: スタイルの指定が効かないバグを修正

--- src/components/TplH1.tsx
+++ src/components/TplH1.tsx
@@ -3,9 +3,10 @@
 import React from "react";
 import styled from "styled-components";
 
+const H1 = styled.h1`
+  text-align: center;
+`;
+
 export const TplH1 = () => {
-  const H1 = styled.h1`
-    text-align: center;
-  `;
   return <H1>2024年北斗祭管理者ページ</H1>;
 };
