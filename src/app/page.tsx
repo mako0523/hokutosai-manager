0693dc617807976128abbd85139f967811427900
mako0523 <kurimako.0523@gmail.com>
2024-05-02 21:34:29 +0900

    feat: 見出しを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
+import { TplH1 } from "../components/TplH1";
 
 export default function Home() {
   return (
     <>
-      <div>北斗祭管理者ページ</div>
+      <TplH1 />
     </>
   );
 }
---
+++ src/components/TplH1.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+export const TplH1 = () => {
+  const H1 = styled.h1`
+    text-align: center;
+  `;
+  return <H1>2024年北斗祭管理者ページ</H1>;
+};
