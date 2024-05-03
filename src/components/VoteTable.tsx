6a39745f26d30a1563ab5cc2fa99ae0b885421ed
mako0523 <kurimako.0523@gmail.com>
2024-05-03 20:28:14 +0900

    fix: 画面全体を中央揃えに変更

--- src/components/VoteTable.tsx
+++ src/components/VoteTable.tsx
@@ -10,12 +10,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-const StyledVoteTable = styled.div`
-  width: 80%;
-  max-width: 800px;
-  margin: 0 auto;
-`;
-
 type VoteTableProps = {
   voteName: string;
 };
@@ -25,30 +19,28 @@ export const VoteTable = async (props: VoteTableProps) => {
   const votes = await fetchVotes(voteName);
 
   return (
-    <StyledVoteTable>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 300 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>インデックス</TableCell>
-              <TableCell align="right">名前</TableCell>
-              <TableCell align="right">投票数</TableCell>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>インデックス</TableCell>
+            <TableCell align="right">名前</TableCell>
+            <TableCell align="right">投票数</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {votes.map((vote, index) => (
+            <TableRow key={index}>
+              <TableCell component="th" scope="row">
+                {index + 1}
+              </TableCell>
+              <TableCell align="right">{vote.name}</TableCell>
+              <TableCell align="right">{vote.count}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {votes.map((vote, index) => (
-              <TableRow key={index}>
-                <TableCell component="th" scope="row">
-                  {index + 1}
-                </TableCell>
-                <TableCell align="right">{vote.name}</TableCell>
-                <TableCell align="right">{vote.count}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </StyledVoteTable>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
