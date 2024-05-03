73706e63e18a7257829d63c6e8b856521a0f11b0
mako0523 <kurimako.0523@gmail.com>
2024-05-03 19:26:28 +0900

    fix: ファイル名を変更

---
+++ src/components/H1.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  text-align: center;
+`;
+
+export const H1 = () => {
+  return <StyledH1>2024年北斗祭管理者ページ</StyledH1>;
+};
