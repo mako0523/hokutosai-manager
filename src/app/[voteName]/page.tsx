eca60e859856c2619d32bdcc0c40e4ff00874b8e
mako0523 <kurimako.0523@gmail.com>
2024-05-04 10:08:19 +0900

    feat: 投票数を確認するページを作成

---
+++ src/app/[voteName]/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { VoteTable } from "../../components/VoteTable";
+import { voteNames, VoteName, toJapaneseVoteName } from "../../utils/vote-name";
+
+type VotePageProps = {
+  params: {
+    voteName: string;
+  };
+};
+
+const votePage = (props: VotePageProps) => {
+  const {
+    params: { voteName },
+  } = props;
+
+  if (!voteNames.includes(voteName as VoteName)) {
+    return <div>存在しないページです．</div>;
+  }
+
+  const japaneseVoteName = toJapaneseVoteName(voteName as VoteName);
+
+  return (
+    <>
+      <h2>「{japaneseVoteName}」の投票数</h2>
+      <VoteTable voteName={voteName} />
+    </>
+  );
+};
+
+export default votePage;
---
+++ src/utils/vote-name.ts
@@ -0,0 +1,27 @@
+export const voteNames = [
+  "stall",
+  "exhibition",
+  "voice",
+  "cosplay",
+  "muscle",
+  "karaoke",
+] as const;
+
+export type VoteName = (typeof voteNames)[number];
+
+export const toJapaneseVoteName = (voteName: VoteName): string => {
+  switch (voteName) {
+    case "stall":
+      return "模擬店投票";
+    case "exhibition":
+      return "展示投票";
+    case "voice":
+      return "Nice voice コンテスト";
+    case "cosplay":
+      return "仮装コンテスト";
+    case "muscle":
+      return "キン肉マンコンテスト";
+    case "karaoke":
+      return "カラオケコンテスト";
+  }
+};
