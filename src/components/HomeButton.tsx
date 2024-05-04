aef9337980948a3469ba32d086091610e8d6f99f
mako0523 <kurimako.0523@gmail.com>
2024-05-04 23:39:15 +0900

    fix: ボタンの中央揃えができていなかったので修正

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,23 +1,21 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
-import { Button } from "@mui/material";
-
-const StyledHomeButton = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 32px;
-`;
+import { Button, Container } from "@mui/material";
 
 export const HomeButton = () => {
   return (
-    <>
-      <StyledHomeButton>
-        <Button variant="contained" href="/">
-          ホームへ戻る
-        </Button>
-      </StyledHomeButton>
-    </>
+    <Container sx={{ display: "flex", justifyContent: "center" }}>
+      <Button
+        variant="contained"
+        href="/"
+        size="large"
+        sx={{
+          marginTop: "32px",
+        }}
+      >
+        ホームへ戻る
+      </Button>
+    </Container>
   );
 };
