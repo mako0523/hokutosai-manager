b12f2361ad72cd176aba568849f1f919736d3ffa
mako0523 <kurimako.0523@gmail.com>
2024-05-04 11:55:27 +0900

    feat: 投票リストコンポーネントを作成

---
+++ src/components/VoteList.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { Button, Link } from "@mui/material";
+import { voteNames, toJapaneseVoteName } from "../utils/vote-name";
+
+const StyledVoteList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const VoteList = () => {
+  return (
+    <StyledVoteList>
+      {voteNames.map((voteName, index) => {
+        return (
+          <Link
+            key={index}
+            component={Button}
+            underline="none"
+            href={`/${voteName}`}
+          >
+            {toJapaneseVoteName(voteName)}
+          </Link>
+        );
+      })}
+    </StyledVoteList>
+  );
+};
