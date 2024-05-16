53558daaaba929548d71f041137c116f1331f965
mako0523 <kurimako.0523@gmail.com>
2024-05-16 12:05:36 +0900

    feat: Basic認証をするための設定ファイルを追加

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/", "/index"],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    if (typeof authValue === "undefined") return;
+
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === "4dmin" && pwd === "testpwd123") {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
+}
